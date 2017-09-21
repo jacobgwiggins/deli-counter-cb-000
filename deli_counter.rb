7426be4e0d53f95e04cd8413a903c7411f982de9
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-21 21:48:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,15 @@ def line(deli)
 
 end
 end
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving(deli)
+  if deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}."
+  end
+end
