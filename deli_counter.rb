1fb878fa96729b82455335e403c4af6d5a446f2d
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-21 20:54:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(deli)
+  counter = 1
+  if deli == []
+    puts "The line is currently empty."
+  else
+    # logic to add number to array elements before pulling for string
+    # puts "The line is currently: #{deli.join(deli.index + 1 + ". ")}"
+
+      puts "The line is currently: #{count}" + deli.each_index.to_s { |x|  (x+1)} + deli.each{|x| print x}
+end
+end
