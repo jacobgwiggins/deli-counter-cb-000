60394cb73e6bccae5df82d72c0f1089349b1f43d
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-21 21:05:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,9 @@ def line(deli)
   else
     # logic to add number to array elements before pulling for string
     # puts "The line is currently: #{deli.join(deli.index + 1 + ". ")}"
+      stringarray = deli.collect { |x| "#{counter}. "+ x}
 
       puts "The line is currently: #{count}" + deli.each_index.to_s { |x|  (x+1)} + deli.each{|x| print x}
+      counter += 1
 end
 end
