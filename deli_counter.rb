fbc1f25c6945b2dd78cb63a96f0af723233ad330
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-21 21:14:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(deli)
   if deli == []
     puts "The line is currently empty."
   else
-    counter = 1
+    
     # logic to add number to array elements before pulling for string
     # puts "The line is currently: #{deli.join(deli.index + 1 + ". ")}"
-      stringarray = deli.collect { |x| " #{counter}. "+ x,
+      stringarray = deli.map {counter = 1, |x| " #{counter}. "+ x,
     counter = counter + 1}
 
       puts "The line is currently:#{stringarray.join}"
